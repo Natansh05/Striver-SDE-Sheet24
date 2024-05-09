e8aaad3f3f7492dd812faba1162716d0d273d29c
natansh05 <202201445@daiict.ac.in>
2024-05-10 00:26:10 +0530

    added optimised solution for set matrix zero

--- Day1 Array/73. Set Matrix Zero.cpp
+++ Day1 Array/73. Set Matrix Zero.cpp
@@ -30,3 +30,69 @@ public:
         return;
     }
 };
+
+
+
+
+
+// Space Optimised Solution 
+// Time Complexity - O(2N)
+// Space Complexity - o(1)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        // to get the optimal approach code here
+
+        // we will use the first row of matrix ax coloumn flag - matrix[0][..]
+        // similarly we will use the first coloumn as row flag - matrix[..][0]
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int col0 = matrix[0][0];
+
+        for(int i=0;i<n;++i){
+            if(matrix[0][i]==0)
+                matrix[0][0]= 0;
+        }
+        for(int i=1;i<m;++i){
+            if(matrix[i][0]==0)
+                col0 = 0;
+        }
+
+
+        for(int i=1;i<m;++i){
+            for(int j=1;j<n;++j){
+                if(matrix[i][j]==0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for(int i = m-1;i>0;--i){
+            for(int j = n-1;j>0;--j){
+                if(matrix[0][j]==0 || matrix[i][0]==0)
+                    matrix[i][j]=0;
+            }
+        }
+
+
+        for(int j=0;j<n;++j){
+            if(matrix[0][0]==0){
+                matrix[0][j]=0;
+            }
+        }
+
+        for(int j=0;j<m;++j){
+            if(col0==0){
+                matrix[j][0]=0;
+            }
+        }
+
+        
+
+        // return matrix;
+    }
+};
