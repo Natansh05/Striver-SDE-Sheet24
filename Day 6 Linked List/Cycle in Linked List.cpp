8d3ad9985f6dc1658b845ce2dcf98df3d4b19a77
natansh05 <202201445@daiict.ac.in>
2024-05-13 16:27:01 +0530

    Create Cycle in Linked List.cpp

---
+++ Day 6 Linked List/Cycle in Linked List.cpp
@@ -0,0 +1,24 @@
+// link - https://leetcode.com/problems/linked-list-cycle/description/
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ios_base::sync_with_stdio(false);
+        if (head==nullptr || head->next==nullptr)
+            return false;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+    
+    while (slow != fast) {
+        if (fast == nullptr || fast->next == nullptr)
+            return false;
+        
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return true;
+    }
+};
