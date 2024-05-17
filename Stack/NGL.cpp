e69d78329340c20d41fef7663e7842f1a3d9f4ae
natansh05 <202201445@daiict.ac.in>
2024-05-17 11:40:49 +0530

    Create NGL.cpp

---
+++ Stack/NGL.cpp
@@ -0,0 +1,31 @@
+// link - https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> previousLargerElement(vector<long long> nums, int n){
+        vector<long long> ans(n,0);
+        stack<long long> st;
+        for(int i=0;i<n;++i){
+            while(1){
+                if(st.empty()){
+                    ans[i]= -1;
+                    st.push(nums[i]);
+                    break;
+                }
+                else if(st.top()>nums[i]){
+                    ans[i] = st.top();
+                    st.push(nums[i]);
+                    break;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return ans;
+    }
+}
