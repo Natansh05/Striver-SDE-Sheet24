c5e42fcef765012b7c88581b92dfff093b234d14
natansh05 <202201445@daiict.ac.in>
2024-05-25 17:14:21 +0530

    Update Power Set.cpp

--- Recursion/Power Set.cpp
+++ Recursion/Power Set.cpp
@@ -1,6 +1,6 @@
 // link - https://www.geeksforgeeks.org/problems/power-set4302/1
 // Time Complexity - O(N*2^N)
-// Space Complexity - O(2^N)
+// Space Complexity - O(N*2^N)
 
 class Solution{
     void solve(string ip,string op,int size,vector<string> &ans){
