82e10504faacdfb7d103e2b3ff9eeb64d475f0e5
natansh05 <202201445@daiict.ac.in>
2024-05-25 12:20:58 +0530

    Create Height of Binary Tree.cpp

---
+++ Recursion/Height of Binary Tree.cpp
@@ -0,0 +1,16 @@
+// Time Complexity - O(H)
+// Space Complexity - O(max(left subtree , right subtree))
+
+
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        if(node==NULL)
+            return 0;
+        int left = height(node->left);
+        int right = height(node->right);
+        
+        return 1 + max(left,right);
+    }
+};
